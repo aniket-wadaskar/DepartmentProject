bb81dea8afdbbfdb7f67dd5a8ffa30757305037e
aniket-wadaskar <arwadaskar2157@gmail.com>
2023-10-20 10:05:50 +0530

    service class commit

--- src/main/java/com/cg/service/DepartmentService.java
+++ src/main/java/com/cg/service/DepartmentService.java
@@ -9,6 +9,30 @@ import com.cg.entity.Department;
 import com.cg.repository.DepartmentRepository;
 
 public class DepartmentService implements DepartmentDao {
-}
+	 @Autowired
+	    private DepartmentRepository departmentRepository;
+	 
+	    // save operation
+	    @Override
+	    public Department saveDepartment(Department department) {
+	        return departmentRepository.save(department);
+	    }
+	 
+	    // read operation
+	    @Override
+	    public List<Department> fetchDepartmentList() {
+	        return (List<Department>) departmentRepository.findAll();
+	    }
+	 
+	   
+	 
+	    // delete operation
+	    @Override
+	    public void deleteDepartmentById(Long departmentId) {
+	        departmentRepository.deleteById(departmentId);
+	    }
+	 
+	}
+
 
 
