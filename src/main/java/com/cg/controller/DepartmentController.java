81c2feb67522498791523cab287de2fe75265ee8
aniket-wadaskar <arwadaskar2157@gmail.com>
2023-10-20 10:13:19 +0530

    controller class commit

--- src/main/java/com/cg/controller/DepartmentController.java
+++ src/main/java/com/cg/controller/DepartmentController.java
@@ -1,5 +1,53 @@
 package com.cg.controller;
 
-public class DepartmentController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.entity.Department;
+import com.cg.service.DepartmentService;
+
+@RestController
+public class DepartmentController {
+	 // Annotation
+    @Autowired private DepartmentService departmentService;
+ 
+    // Save operation
+    @PostMapping("/departments")
+    public Department saveDepartment(
+         @RequestBody Department department)
+    {
+ 
+        return departmentService.saveDepartment(department);
+    }
+ 
+    // Read operation
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList()
+    {
+ 
+        return departmentService.fetchDepartmentList();
+    }
+ 
+   
+ 
+    // Delete operation
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id")
+                                       Long departmentId)
+    {
+ 
+        departmentService.deleteDepartmentById(
+            departmentId);
+        return "Deleted Successfully";
+    }
+	
 }
