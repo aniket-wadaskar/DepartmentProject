f5b4bd6272c6126af33c0780bfd9d3e6aa2c9a09
aniket-wadaskar <arwadaskar2157@gmail.com>
2023-10-20 09:50:37 +0530

    dao class commit

--- src/main/java/com/cg/dao/DepartmentDao.java
+++ src/main/java/com/cg/dao/DepartmentDao.java
@@ -1,5 +1,15 @@
 package com.cg.dao;
 
-public interface DepartmentDao {
+import java.util.List;
 
+import com.cg.entity.Department;
+
+public interface DepartmentDao {
+	
+	Department saveDepartment(Department department); // save the department
+	
+	List<Department> fetchDepartmentList();           // Read the list 
+	
+	void deleteDepartmentById(Long departmentId); // Delete the department
+                          
 }
