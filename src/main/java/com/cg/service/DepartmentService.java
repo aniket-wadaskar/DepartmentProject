b8112695960570127f0caa42ed1ec04981c64a74
aniket-wadaskar <arwadaskar2157@gmail.com>
2023-10-20 10:08:55 +0530

    service class commit

--- src/main/java/com/cg/service/DepartmentService.java
+++ src/main/java/com/cg/service/DepartmentService.java
@@ -3,11 +3,13 @@ package com.cg.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cg.dao.DepartmentDao;
 import com.cg.entity.Department;
 import com.cg.repository.DepartmentRepository;
 
+@Service
 public class DepartmentService implements DepartmentDao {
 	 @Autowired
 	    private DepartmentRepository departmentRepository;
